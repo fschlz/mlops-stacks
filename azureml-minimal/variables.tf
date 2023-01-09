aca29e31e47440c5d048aa97ca88d04502a9f8dc
Francisco Schulz <22344801+fschlz@users.noreply.github.com>
2023-01-09 09:22:43 +0100

    update zenml version

--- azureml-minimal/variables.tf
+++ azureml-minimal/variables.tf
@@ -1,6 +1,6 @@
 # variables for creating a ZenML stack configuration file
 variable "zenml-version" {
   description = "The version of ZenML being used"
-  default     = "0.20.1"
+  default     = "0.31.0"
   type        = string
 }
